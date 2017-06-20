db1f6e1858d1d3448aeff0805003ba2ded1fcc71
pro <pro@auth40-161.rz.hs-offenburg.de>
2017-06-20 11:17:10 +0200

    adapted for flush and reload

--- plot.py
+++ plot.py
@@ -1,10 +1,7 @@
 import struct
 import statistics as s
-import numpy as np
-import numpy.random
-import matplotlib.pyplot as plt
-import sphviewer as sph
-data=[[] for x in xrange(5)]
+
+data=[[] for x in xrange(2)]
 x = []
 y= []
 def readChunks():
@@ -25,41 +22,12 @@ def inc():
 
 for num,delay in zip(inc(),readChunks()):
     if delay < 4000:
-        data[num % 5].append((num % 5,delay))
+        data[num % 2].append((num % 2,delay))
         x.append(num % 5)
         y.append(delay)
     
-#for row in data:
-#    print s.mean(row[1])
-def myplot(x, y, nb=32, xsize=500, ysize=500):   
-    xmin = np.min(x)
-    xmax = np.max(x)
-    ymin = np.min(y)
-    ymax = np.max(y)
+for row in data:
+    print s.mean(row[1])
 
-    x0 = (xmin+xmax)/2.
-    y0 = (ymin+ymax)/2.
-
-    pos = np.zeros([3, len(x)])
-    pos[0,:] = x
-    pos[1,:] = y
-    w = np.ones(len(x))
-
-    P = sph.Particles(pos, w, nb=nb)
-    S = sph.Scene(P)
-    S.update_camera(r='infinity', x=x0, y=y0, z=0, 
-                    xsize=xsize, ysize=ysize)
-    R = sph.Render(S)
-    R.set_logscale()
-    img = R.get_image()
-    extent = R.get_extent()
-    for i, j in zip(xrange(4), [x0,x0,y0,y0]):
-        extent[i] += j
-    print extent
-    return img, extent
-
-heatmap_16, extent_16 = myplot(x,y, nb=16)
-plt.imshow(heatmap_16, extent=extent_16, origin='lower', aspect='auto')
-plt.show
 
     
